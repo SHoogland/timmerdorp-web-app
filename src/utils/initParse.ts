3c0da126d433e8f3adca778e048f3e9ce371ce50
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2024-08-10 22:37:50 -0400

    undo temporary change

--- src/utils/initParse.ts
+++ src/utils/initParse.ts
@@ -4,10 +4,10 @@ import redirectToLogin from './redirectToLogin.ts';
 
 const initParse = async () => {
 	Parse.initialize(
-		'myAppId',
-		'jsKey'
+		import.meta.env.VITE_APP_ID,
+		import.meta.env.VITE_JS_KEY
 	);
-	Parse.serverURL = 'http://localhost:1337/1'
+	Parse.serverURL = import.meta.env.VITE_PARSE_URL
 
 	const currentUser = Parse.User.current();
 	if (currentUser) {
