82d8c20b72b095294311596f7f9e89472d1cb5ca
Stan van Baarsen <stanvanbaarsen@hotmail.com>
2024-08-15 10:49:08 -0400

    add ios capacitor platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.shoogland.timmerdorp',
+  appName: 'timmerdorp-web-app',
+  webDir: 'dist'
+};
+
+export default config;
